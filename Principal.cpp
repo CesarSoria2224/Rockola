5e999aad63ab2a940e53e2a487ffcb865066ea4d
CesarSoria2224 <130561780+CesarSoria2224@users.noreply.github.com>
2023-06-19 20:44:52 -0400

    Add files via upload

--- Principal.cpp
+++ Principal.cpp
@@ -1,8 +1,4 @@
 #include "RockolaManager.cpp"
-#include "CancionManejadorDatosEstaticos.cpp"
-#include "CancionManejadorDatosArchivo.cpp"
-#include "ArtistaManejadorDatosEstaticos.cpp"
-#include "ArtistaManejadorDatosArchivo.cpp"
 #include <iostream>
 using namespace std;
 
@@ -10,63 +6,65 @@ using namespace std;
 int main() {
     RockolaManager rockola;
     //rockola.loadData();
-    DataManager *dm;
-    CancionManejadorDatos *a;
-    ArtistaManejadorDatos *b;
+   /* DataManager* dm;
+    CancionManejadorDatos* a;
+    ArtistaManejadorDatos* b;*/
+
+    int opcion;
+     do{
+    cout << "Seleccione el tipo de dato a trabajar:" << endl;
+    cout << "1. Datos estaticos" << endl;
+    cout << "2. Datos desde archivo de texto" << endl;
+    cout << "Ingrese su opcion: ";
+    cin >> opcion;
+
+    if (opcion == 1) {
+        // Trabajar con datos estáticos
+        rockola.IniciarDatoEstatico();
+    } else if (opcion == 2) {
+        // Trabajar con datos desde archivo de texto
+        rockola.IniciarDatoArchivo();
+    } else {
+        cout << "Opcion invalida. Saliendo del programa." << endl;
+    } 
+    }while (opcion != 3);
 
      //int opcion;
-     //cout << "Seleccione el tipo de dato a trabajar:" << endl;
-     //cout << "1. Datos estaticos" << endl;
-     //cout << "2. Datos desde archivo de texto" << endl;
-     //cout << "Ingrese su opcion: ";
-     //cin >> opcion;
 
-     //if (opcion == 1) {
-     //    // Trabajar con datos estáticos
-     //    rockola.IniciarDatoEstatico();
-     //} else if (opcion == 2) {
-     //    // Trabajar con datos desde archivo de texto
-     //    rockola.IniciarDatoArchivo();
-     //} else {
-     //    cout << "Opcion invalida. Saliendo del programa." << endl;
-     //}
+     //do {
+     //    cout << "Selecciona el tipo de fuente de datos:" << endl;
+     //    cout << "1) Estático" << endl;
+     //    cout << "2) Archivo TXT" << endl;
+     //    cout << "0) Salir" << endl;
+     //    cout << "Opción: ";
+     //    cin >> opcion;
 
-     int opcion;
+     //    switch (opcion) {
+     //    case 1:
+     //        a = new CancionManejadorDatosEstaticos();
+     //        b = new ArtistaManejadorDatosEstaticos();
+     //        // Realiza las operaciones con los datos estáticos
+     //        // ...
+     //        break;
+     //    case 2:
+     //        a = new CancionManejadorDatosArchivo();
+     //        b = new ArtistaManejadorDatosArchivo();
+     //        // Realiza las operaciones con los datos del archivo TXT
+     //        // ...
+     //        break;
+     //    case 0:
+     //        cout << "Saliendo del programa..." << endl;
+     //        break;
+     //    default:
+     //        cout << "Opción inválida. Inténtalo de nuevo." << endl;
+     //        break;
+     //    }
 
-     do {
-         cout << "Selecciona el tipo de fuente de datos:" << endl;
-         cout << "1) Estático" << endl;
-         cout << "2) Archivo TXT" << endl;
-         cout << "0) Salir" << endl;
-         cout << "Opción: ";
-         cin >> opcion;
+     //    // Liberar la memoria asignada a los objetos a y b
+     //    delete a;
+     //    delete b;
 
-         switch (opcion) {
-         case 1:
-             a = new CancionManejadorDatosEstaticos();
-             b = new ArtistaManejadorDatosEstaticos();
-             // Realiza las operaciones con los datos estáticos
-             // ...
-             break;
-         case 2:
-             a = new CancionManejadorDatosArchivo();
-             b = new ArtistaManejadorDatosArchivo();
-             // Realiza las operaciones con los datos del archivo TXT
-             // ...
-             break;
-         case 0:
-             cout << "Saliendo del programa..." << endl;
-             break;
-         default:
-             cout << "Opción inválida. Inténtalo de nuevo." << endl;
-             break;
-         }
-
-         // Liberar la memoria asignada a los objetos a y b
-         delete a;
-         delete b;
-
-     } while (opcion != 0);
+     //} while (opcion != 0);
      /*
      Seleccion el tipo de fuente datos
      1) Estico
